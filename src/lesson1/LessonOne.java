a51e49026f46b7d2597052d32f3508450dd3670e
SiarheiKudayeu <102074437+SiarheiKudayeu@users.noreply.github.com>
2023-10-10 21:01:58 +0300

    Update LessonOne.java

--- src/lesson1/LessonOne.java
+++ src/lesson1/LessonOne.java
@@ -4,5 +4,6 @@ public class LessonOne {
     public static void main(String[] args) {
         System.out.println("Hello QA-6");
         System.out.println("Good morning");
+        System.out.println("Good evening");
     }
 }
